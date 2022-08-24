aff51573405efe621431a51b6191dd252112779b
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2022-08-24 20:35:51 +0200

    vim-plug

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ git clone https://github.com/rust-lang/rust.vim ~/.vim/pack/plugins/start/rust.v
 # NerdTree
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+
+# Vim Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
